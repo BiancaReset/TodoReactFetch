e2613fe179039470b3bd748e0f5d716115f7065b
BiancaReset <130721122+BiancaReset@users.noreply.github.com>
2023-08-01 16:18:35 +0000

    1agosto

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,86 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from 'react';
 
-const API_URL = 'https://playground.4geeks.com/apis/fake/todos/';
+const TodoList = () => {
+  const [listItem, setListItem] = useState([]);
+  const inputRef = useRef(null);
 
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+  function addTodo() {
+    const value = inputRef.current.value.trim();
+    if (value === '') {
+      alert('Todo cannot be empty');
+    } else {
+      setListItem([...listItem, { label: value, done: false }]);
+      inputRef.current.value = '';
+    }
+  }
+
+  async function deleteAllTodos() {
+    setListItem([]);
+  }
 
   useEffect(() => {
-    fetchTodos();
+    updatetodo();
   }, []);
 
-  const fetchTodos = async () => {
-    try {
-      const response = await axios.get(`${API_URL}/user/BiancaReset`);
-      setTodos(response.data.result); 
-    } catch (error) {
-      console.error('Error fetching todos:', error);
+  useEffect(() => {
+    newtodo();
+  }, [listItem]);
+
+  async function updatetodo() {
+    let response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/BiancaReset');
+    if (!response.ok) {
+      console.error('error');
+      return;
     }
+    let data = await response.json();
+    setListItem(data);
+  }
+
+  async function newtodo() {
+    if (listItem.length !== 0) {
+      const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(listItem),
+      };
+      await fetch('https://playground.4geeks.com/apis/fake/todos/user/BiancaReset', requestOptions);
+    }
+  }
+
+  const deleteItem = (deleteItem) => {
+    console.log(deleteItem);
+    const newList = listItem.filter((el) => el.label !== deleteItem.label);
+    setListItem(newList);
+    console.log(listItem);
   };
 
-  const handleInputChange = (event) => {
-    setNewTodo(event.target.value);
-  };
-
-  const addTodo = async () => {
-    if (!newTodo.trim()) return;
-  
-    try {
-      const response = await axios.post(`${API_URL}/user/BiancaReset`, {
-        label: newTodo,
-        done: false,
-      });
-      setNewTodo('');
-      fetchTodos(); // Refresh the todo list after adding a new todo
-    } catch (error) {
-      console.error('Error adding todo:', error);
-    }
-  };
-
-  const updateTodoList = async () => {
-    try {
-      await axios.put(`${API_URL}/user/BiancaReset`, todos);
-      console.log('Todo list updated successfully');
-    } catch (error) {
-      console.error('Error updating todo list:', error);
-    }
-  };
-
-  const deleteTodo = async (index) => {
-    try {
-      const updatedTodos = [...todos];
-      updatedTodos.splice(index, 1);
-      setTodos(updatedTodos);
-      await updateTodoList();
-    } catch (error) {
-      console.error('Error deleting todo:', error);
-    }
-  };
+  let lista = listItem.map((el, index) => (
+    <li key={index}>
+      {el.label} <button onClick={() => deleteItem(el)}>Delete</button>
+    </li>
+  ));
 
   return (
-    <div className="todo-list">
-      <h1>Todo List</h1>
-      <div className="todo-container">
-        <input
-          type="text"
-          placeholder="Add a new todo..."
-          value={newTodo}
-          onChange={handleInputChange}
-        />
-        <button onClick={addTodo}>Add</button>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={index}>
-              <span>{todo.label}</span>
-              <button onClick={() => deleteTodo(index)}>Delete</button>
-            </li>
-          ))}
-        </ul>
+    <div className="">
+      <input type="text" ref={inputRef} />
+      <button onClick={addTodo}>Add Todo</button>
+      <button onClick={deleteAllTodos}>Delete All Todos</button>
+      <div>
+        <ul>{listItem.length === 0 ? 'no hay tareas' : lista}</ul>
       </div>
     </div>
   );
-}
+};
 
 export default TodoList;
